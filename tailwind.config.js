9f9dfbd3532b37822e6a9529e8ab3d52062b61de
dannylindquist <danny@fromdl.com>
2024-12-24 12:44:20 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,ts}', './index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        native: ['Labrada', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
